641f1386b382710c2f994dda955ffd80481a0267
David Lambert <david.lambert@my.utsa.edu>
2022-08-19 11:59:30 -0500

    added timer buttons and class

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("HI there")
+class Timer{
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput
+        this.startButton = startButton
+        this.pauseButton = pauseButton
+
+        //clicks start button, start method called
+        this.startButton.addEventListener('click', this.start)
+    }
+
+    start(){
+        console.log("timer started")
+    }
+}
+// # is how to select ID's
+const durationInput = document.querySelector('#duration')
+const startButton = document.querySelector('#start')
+const pauseButton = document.querySelector('#pause')
+
+const timer = new Timer(durationInput, startButton, pauseButton)
