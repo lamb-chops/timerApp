b8293368f5965dc7736723c9dfc73d7e3c7994bb
David Lambert <david.lambert@my.utsa.edu>
2022-08-20 09:37:35 -0500

    refactored timer class to own file

--- index.js
+++ index.js
@@ -1,46 +1,20 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    //clicks start button, start method called
-    this.startButton.addEventListener("click", this.start);
-
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-
-  start = () => {
-    console.log("timer started");
-    //calls tick() every second, call it first so dont have to wait a full second before countdown
-    this.tick();
-    //this.timer allows it to be accessed elsewhere
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-    console.log("timer paused");
-  };
-
-  tick = () => {
-    //how to call setter and getter
-    this.timeRemaining = this.timeRemaining - 1
-  };
-
-  //how to write getter, acts like variable
-  get timeRemaining() {
-    //parse float converts string to decimal
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
 // # is how to select ID's
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+//callbacks in {...} which is object
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  //call backs signal to world something happened
+  onStart() {
+    console.log("timer started");
+  },
+
+  onTick() {
+    console.log("tick");
+  },
+
+  onComplete() {
+    console.log("timer paused");
+  },
+});
---
+++ timer.js
@@ -0,0 +1,58 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    //checks if any callbacks passed
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+    //clicks start button, start method called
+    this.startButton.addEventListener("click", this.start);
+
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    //check if defined first
+    if (this.onStart) {
+      this.onStart();
+    }
+    //calls tick() every second, call it first so dont have to wait a full second before countdown
+    this.tick();
+    //this.timer allows it to be accessed elsewhere
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      //how to call setter and getter
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
+  };
+
+  //how to write getter, acts like variable
+  get timeRemaining() {
+    //parse float converts string to decimal
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
