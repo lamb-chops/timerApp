86fc3733ba7b0fdae119a2f41b88489cc3ae9eac
David Lambert <david.lambert@my.utsa.edu>
2022-08-20 08:43:00 -0500

    got countdown method functioning

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-class Timer{
-    constructor(durationInput, startButton, pauseButton){
-        this.durationInput = durationInput
-        this.startButton = startButton
-        this.pauseButton = pauseButton
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
 
-        //clicks start button, start method called
-        this.startButton.addEventListener('click', this.start)
-    }
+    //clicks start button, start method called
+    this.startButton.addEventListener("click", this.start);
+  }
 
-    start(){
-        console.log("timer started")
-    }
+  start = () => {
+    console.log("timer started");
+    //calls tick() every second, call it first so dont have to wait a full second before countdown
+    this.tick();
+    setInterval(this.tick, 1000);
+  };
+
+  tick = () => {
+    console.log("tick");
+  };
 }
 // # is how to select ID's
-const durationInput = document.querySelector('#duration')
-const startButton = document.querySelector('#start')
-const pauseButton = document.querySelector('#pause')
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton)
+const timer = new Timer(durationInput, startButton, pauseButton);
